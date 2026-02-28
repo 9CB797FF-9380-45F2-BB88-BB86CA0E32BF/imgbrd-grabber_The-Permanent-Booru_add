40ff9ae40631d3af5dbb4f3d5b4f965b79306140
John Hong <57867081+9CB797FF-9380-45F2-BB88-BB86CA0E32BF@users.noreply.github.com>
2026-03-01 07:12:27 +0900

    Add JSON API
    add json API

--- src/sites/The Permanent Booru/model.ts
+++ src/sites/The Permanent Booru/model.ts
@@ -28,6 +28,7 @@
  * /post/<postID> -> image select by post number
  * /post/md5/<image_md5> -> image select by md5
  * /post/sha256/<image_sha256> -> image select by sha256
+ * /api/v1/posts -> Image List API(JSON), more info:https://github.com/Kycklingar/pbooru-downloader
 */
 
 /* This booru sets the number of images per page as OPTION method instead of passing it as GET method.
@@ -36,51 +37,29 @@
 
 // Sometimes comments on a line are taken from another model.js, but it is not clear what they are used for.
 
-// -------- Typescript Helper Start --------
+// -------- Typescript Type Narrowing Start --------
 
 // Check if an object is an ITag interface
 function isITagArr(obj: any): obj is ITag[]{
-    return "name" in obj;
+    return obj.every((i: any) => i.name);
 }
 
 // Check if an object is an String Array
 function isStringArr(obj: any): obj is string[] {
-    return typeof obj === 'object';
+    return obj.every((i: any) => typeof i === "string");
 }
 
 // Check if an object is an String
 function isString(obj: any): obj is string {
-    return typeof obj === 'string';
+    return typeof obj === "string";
 }
 
-// -------- Typescript Helper End --------
+// -------- Typescript Type Narrowing End --------
 
 
-// -------- Custom Function Start --------
+// -------- Static Map Start --------
 
-// Convert to be compatible with existing tag DB
-function tag_refinement(images: IImage): void {
-    if (images.tags) {
-        if (isString(images.tags)) {
-            let vaild_tag_check_temp = images.tags.toString();
-            if (vaild_tag_check_temp === "null") { // Image tag Not existing image
-                images.tags = []; // If tags is (null), the image will not be displayed. Therefore, it is set to an empty list.
-            }
-            else {
-                vaild_tag_check_temp = recover_html_entity(vaild_tag_check_temp); // convert HTML entity to special characters
-                images.tags = JSON.parse('{"tags":'+vaild_tag_check_temp+'}')["tags"];
-            }
-        }
-        // I don't think ITag[] will ever be used as an argument in this function.
-        /*else if (isITagArr(images.tags)) {
-            for(let tag_index=0; tag_index < images.tags.length; tag_index++) {
-                images.tags[tag_index].name = images.tags[tag_index].name; 
-            }
-        }*/
-    }
-}
-
-// Convert Tag Type and Tag Type ID to be compatible with Grabber
+// tag type name -> tag type ID
 const TAG_NAME_TO_TTYPE_ID_MAP: Record<string, any> = {
     "none": 0,
     "rating": 1,
@@ -92,6 +71,8 @@ const TAG_NAME_TO_TTYPE_ID_MAP: Record<string, any> = {
     "creator": 7,
     "character": 8
 };
+
+// tag namespace -> tag type name
 const TAG_NAME_TO_GRB_TTYPE_MAP: Record<string, any> = {
     "none": "general",
     "rating": "meta",
@@ -103,103 +84,8 @@ const TAG_NAME_TO_GRB_TTYPE_MAP: Record<string, any> = {
     "creator": "artist",
     "character": "character"
 };
-function tag_type_compat(tags: ITag): void {
-    if (typeof tags.typeId === "number") { // type check
-        tags.typeId = TAG_NAME_TO_TTYPE_ID_MAP[tags.typeId] || 0;
-    }
-    if (typeof tags.type === "string") { // type check
-        tags.type = TAG_NAME_TO_GRB_TTYPE_MAP[tags.type] || "general";
-    }
-}
 
-// Fill in placeholders if no preview image is available (ex. swf file)
-function fill_placeholder_preview(images: IImage): void {
-    if (!images.preview_url) {
-        images.preview_url = "/favicon.ico";
-    }
-}
-
-// Returns the extracted tags as a string separated by commas.
-function extracted_tags_to_string(tags: ITag[]): string {
-    let result = String();
-    let tag_temp = String();
-    for (let tag_index = 0; tag_index < tags.length; tag_index++) {
-        tag_temp = tags[tag_index].name.trim(); // Remove left and right spaces
-        tag_temp = tag_temp.replace(/^(~[!|+])\s?/, ''); // Remove prefix, For spaces between prefixes and words, use lazy quantifiers by limiting to 1 in the Grabber.regexToTags function.
-        result = result.concat(tag_temp);
-        if (tag_index < tags.length - 1) {
-            result = result.concat(", ");
-        }
-    }
-    return result;
-}
-
-// Separate search tags
-// Prefix: (no prefix):And, ~|:Or, ~!:Filter, ~+:Unless
-// The Permanent Booru's tags will not be searched as intended if they match the prefixes used by the search keyword parser.
-// The search parser currently appears to be functioning normally, but it may fail due to various tags in The Permanent Booru that do not conform to its rules. Therefore, we need your feedback.
-function search_keyword_parser(search_query: ISearchQuery): Record<string, any>  {
-    const parsed_keyword: Record<string, any> = {};
-    const extracted_tags_and = Grabber.regexToTags("(?<name>(?:^|(?<!\\\\,)(?<=,))(?!\\s*(?:~[+|!]))(?:\\\\,|[^,])+(?:(?=,)|$))", search_query.search); // Hints about AND regular expressions came from generative artificial intelligence(Gemini-3.1-Pro).
-    const extracted_tags_filter = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~!)(?:\\\\,|[^,])+)", search_query.search);
-    const extracted_tags_or = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~\\|)(?:\\\\,|[^,])+)", search_query.search);
-    const extracted_tags_unless = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~\\+)(?:\\\\,|[^,])+)", search_query.search);
-    parsed_keyword["and"] = extracted_tags_to_string(extracted_tags_and);
-    parsed_keyword["filter"] = extracted_tags_to_string(extracted_tags_filter);
-    parsed_keyword["or"] = extracted_tags_to_string(extracted_tags_or);
-    parsed_keyword["unless"] = extracted_tags_to_string(extracted_tags_unless);
-    return parsed_keyword;
-}
-
-// Get the source from the image details page.
-function regexToSources(regexp: string, src: string): string[] {
-    const urls = Grabber.regexMatches(regexp, src);
-    const result = [];
-    for (let url_index = 0; url_index < urls.length; url_index++) {
-        result[url_index] = urls[url_index]["url"];
-    }
-    return result;
-}
-
-/* In case of JavaScript, if <IImage object>.tags, which is information received through Grabber.
- * regexToImages() in search->parse, is a string, Grabber automatically extracts the rating.
- * However, when writing in TypeScript, <IImage object>.tags must be of type string[] or ITag[].
- * If this constraint is met, tags are stored as string[] (e.g., ["tag1", "tag2", "tag3"]).
- * In this case, Grabber does not automatically extract the rating.
-*/
-/* There are many variations of the ratring tag, but in this case we only deal with safe, questionable, and explicit.
- * And when there are multiple levels of rating, the priority is explicit > questionable > safe.
-*/
-// Extract ratring from tags extracted from search->parse.
-function fill_rating_from_string_tag(images: IImage): void {
-    if (images.tags) {
-        if (isStringArr(images.tags)) {
-            if (images.tags.indexOf("rating:safe") > -1) {
-                images.rating = "safe";
-            }
-            if (images.tags.indexOf("rating:questionable") > -1) {
-                images.rating = "questionable";
-            }
-            if (images.tags.indexOf("rating:explicit") > -1) {
-                images.rating = "explicit";
-            }
-        }
-        // I don't think ITag[] will ever be used as an argument in this function.
-        else if (isITagArr(images.tags)) {
-            if (images.tags.filter( (itag) => { return itag.name === "rating:safe"; }).length > 0) {
-                images.rating = "safe";
-            }
-            if (images.tags.filter( (itag) => { return itag.name === "rating:questionable"; }).length > 0) {
-                images.rating = "questionable";
-            }
-            if (images.tags.filter( (itag) => { return itag.name === "rating:explicit"; }).length > 0) {
-                images.rating = "explicit";
-            }
-        }
-    }
-}
-
-// HTML Entity code to Normal Character
+// HTML entity -> Character
 // about this map: https://html.spec.whatwg.org/multipage/named-characters.html
 const html_entity_normal_character_maps: Record<string, string> = {
     "&#198;": "\u00C6", // &AElig
@@ -1649,7 +1535,149 @@ const html_entity_normal_character_maps: Record<string, string> = {
     "&#120015;": "\uD835\uDCCF", // &zscr;
     "&#8205;": "\u200D", // &zwj;
     "&#8204;": "\u200C", // &zwnj;
+};
+
+// jaon <-> iimage
+const imgMap: Record<string, string> = {
+    "id": "ID",
+    "name": "File.Name", // same url+md5+ext, There is a real file name property called "Metadata.filename.0", but it is rarely seen and an error occurs if this property does not exist.
+    "file_url": "File.Url",
+    "file_size": "File.Size",
+    //"sha256": "File.Sha256",
+    "md5": "File.Md5",
+    "date": "File.CreatedDate",
+    "width": "Dimension.Width",
+    "height": "Dimension.Height",
+    "preview_url": "Thumbnails.2.File.Url", // thumbnails size - 0:256px, 1:512px, 2:1024px
+    "preview_width": "Thumbnails.2.File.Url",
+    "preview_height": "Thumbnails.2.File.Url",
+    "sources": "Metadata.source",
+};
+
+// -------- Static Map End --------
+
+
+// -------- Custom Function Start --------
+
+// Convert to be compatible with existing tag DB
+function tag_refinement(images: IImage): void {
+    if (images.tags) {
+        if (isString(images.tags)) {
+            let vaild_tag_check_temp = images.tags.toString();
+            if (vaild_tag_check_temp === "null") { // Image tag Not existing image
+                images.tags = []; // If tags is (null), the image will not be displayed. Therefore, it is set to an empty list.
+            }
+            else {
+                vaild_tag_check_temp = recover_html_entity(vaild_tag_check_temp); // convert HTML entity to special characters
+                images.tags = JSON.parse('{"tags":'+vaild_tag_check_temp+'}')["tags"];
+            }
+        }
+        // I don't think ITag[] will ever be used as an argument in this function.
+        /*else if (isITagArr(images.tags)) {
+            for(let tag_index=0; tag_index < images.tags.length; tag_index++) {
+                images.tags[tag_index].name = images.tags[tag_index].name; 
+            }
+        }*/
+    }
 }
+
+// Convert Tag Type and Tag Type ID to be compatible with Grabber
+function tag_type_compat(tags: ITag): void {
+    if (typeof tags.typeId === "number") { // type check
+        tags.typeId = TAG_NAME_TO_TTYPE_ID_MAP[tags.typeId] || 0;
+    }
+    if (typeof tags.type === "string") { // type check
+        tags.type = TAG_NAME_TO_GRB_TTYPE_MAP[tags.type] || "general";
+    }
+}
+
+// Fill in placeholders if no preview image is available (ex. swf file)
+function fill_placeholder_preview(images: IImage): void {
+    if (!images.preview_url) {
+        images.preview_url = "/favicon.ico";
+    }
+}
+
+// Returns the extracted tags as a string separated by commas.
+function extracted_tags_to_string(tags: ITag[]): string {
+    let result = String();
+    let tag_temp = String();
+    for (let tag_index = 0; tag_index < tags.length; tag_index++) {
+        tag_temp = tags[tag_index].name.trim(); // Remove left and right spaces
+        tag_temp = tag_temp.replace(/^(~[!|+])\s?/, ''); // Remove prefix, For spaces between prefixes and words, use lazy quantifiers by limiting to 1 in the Grabber.regexToTags function.
+        result = result.concat(tag_temp);
+        if (tag_index < tags.length - 1) {
+            result = result.concat(", ");
+        }
+    }
+    return result;
+}
+
+// Separate search tags
+// Prefix: (no prefix):And, ~|:Or, ~!:Filter, ~+:Unless
+// The Permanent Booru's tags will not be searched as intended if they match the prefixes used by the search keyword parser.
+// The search parser currently appears to be functioning normally, but it may fail due to various tags in The Permanent Booru that do not conform to its rules. Therefore, we need your feedback.
+function search_keyword_parser(search_query: ISearchQuery): Record<string, any>  {
+    const parsed_keyword: Record<string, any> = {};
+    const extracted_tags_and = Grabber.regexToTags("(?<name>(?:^|(?<!\\\\,)(?<=,))(?!\\s*(?:~[+|!]))(?:\\\\,|[^,])+(?:(?=,)|$))", search_query.search); // Hints about AND regular expressions came from generative artificial intelligence(Gemini-3.1-Pro).
+    const extracted_tags_filter = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~!)(?:\\\\,|[^,])+)", search_query.search);
+    const extracted_tags_or = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~\\|)(?:\\\\,|[^,])+)", search_query.search);
+    const extracted_tags_unless = Grabber.regexToTags("(?<name>(?<![^,]|\\\\,)(?=\\s*~\\+)(?:\\\\,|[^,])+)", search_query.search);
+    parsed_keyword["and"] = extracted_tags_to_string(extracted_tags_and);
+    parsed_keyword["filter"] = extracted_tags_to_string(extracted_tags_filter);
+    parsed_keyword["or"] = extracted_tags_to_string(extracted_tags_or);
+    parsed_keyword["unless"] = extracted_tags_to_string(extracted_tags_unless);
+    return parsed_keyword;
+}
+
+// Get the source from the image details page.
+function regexToSources(regexp: string, src: string): string[] {
+    const urls = Grabber.regexMatches(regexp, src);
+    const result = [];
+    for (let url_index = 0; url_index < urls.length; url_index++) {
+        result[url_index] = urls[url_index]["url"];
+    }
+    return result;
+}
+
+/* In case of JavaScript, if <IImage object>.tags, which is information received through Grabber.
+ * regexToImages() in search->parse, is a string, Grabber automatically extracts the rating.
+ * However, when writing in TypeScript, <IImage object>.tags must be of type string[] or ITag[].
+ * If this constraint is met, tags are stored as string[] (e.g., ["tag1", "tag2", "tag3"]).
+ * In this case, Grabber does not automatically extract the rating.
+*/
+/* There are many variations of the ratring tag, but in this case we only deal with safe, questionable, and explicit.
+ * And when there are multiple levels of rating, the priority is explicit > questionable > safe.
+*/
+// Extract ratring from tags extracted from search->parse.
+function fill_rating_from_string_tag(images: IImage): void {
+    if (images.tags) {
+        if (isITagArr(images.tags)) {
+            if (images.tags.filter( (itag) => { return itag.name === "rating:safe"; }).length > 0) {
+                images.rating = "safe";
+            }
+            if (images.tags.filter( (itag) => { return itag.name === "rating:questionable"; }).length > 0) {
+                images.rating = "questionable";
+            }
+            if (images.tags.filter( (itag) => { return itag.name === "rating:explicit"; }).length > 0) {
+                images.rating = "explicit";
+            }
+        }
+        else if (isStringArr(images.tags)) {
+            if (images.tags.indexOf("rating:safe") > -1) {
+                images.rating = "safe";
+            }
+            if (images.tags.indexOf("rating:questionable") > -1) {
+                images.rating = "questionable";
+            }
+            if (images.tags.indexOf("rating:explicit") > -1) {
+                images.rating = "explicit";
+            }
+        }
+    }
+}
+
+// HTML Entity code to Normal Character
 // code from https://velog.io/@sisofiy626/JS-HTML-entity와-변환
 function recover_html_entity(html_str: string): string {
         return html_str.replace(/&#\d+;/g, (matched) => html_entity_normal_character_maps[matched] || matched);
@@ -1658,6 +1686,35 @@ function recover_html_entity(html_str: string): string {
 // wrapper traveling the name of the ITag
 function recover_html_entity_itag_wrapper(tag: ITag): void {
     tag.name = recover_html_entity(tag.name);
+}
+
+// Json to IImage
+function jason_to_iimage(image: Record<string, any>): IImage {
+    const img = Grabber.mapFields(image, imgMap);
+    // I haven't figured out yet what "Removed" and "Deleted" do.
+    img.status = "active";
+    if (image.Removed) {
+        img.status = "flagged";
+    }
+    else if (image.Deleted) {
+        img.status = "deleted";
+    }
+    const tags: ITag[] = [];
+    let name = undefined;
+    let type = undefined;
+    let count = undefined;
+    let typeId = undefined;
+    for (const tag_index in image.Tags) {
+        name = (image.Tags[tag_index].Namespace === "none") ? (image.Tags[tag_index].Tag) : (image.Tags[tag_index].Namespace + ':' + image.Tags[tag_index].Tag);
+        typeId = TAG_NAME_TO_TTYPE_ID_MAP[image.Tags[tag_index].Namespace] || 0;
+        type = TAG_NAME_TO_GRB_TTYPE_MAP[image.Tags[tag_index].Namespace] || "general";
+        count = image.Tags[tag_index].Count;
+        tags.push({ name, type, count, typeId });
+    }
+    img.tags = tags;
+    img.ext = Grabber.regexMatch("(?<=\\.)\\w+$", img.name)[0]; // extension from file name
+    img.name = Grabber.regexMatch("(?<=/)\\p{Hex_Digit}{64}", img.name)[0]; // Remove url and extension parts
+    return img;
 }
 
 // -------- Custom Function End --------
@@ -1911,6 +1968,43 @@ export const source: ISource = {
                 },
                 parse: (src: string): boolean => {
                     return src.search('<div class="logo">The Permanent Booru</div>') !== -1;
+                },
+            },
+        },
+        json: {
+            name: "JSON",
+            auth: [],
+            maxLimit: 50,
+            search: {
+                parseErrors: true,
+                url: (query: ISearchQuery, opts: IUrlOptions, previous: IPreviousSearch | undefined): string | IError => {
+                    try {
+                        // Since this booru receives the search query as multiple parameters instead of one, it is necessary to properly separate them and pass them to the appropriate parameters.
+                        // Prefix: (no prefix):And, ~|:Or, ~!:Filter, ~+:Unless
+                        const vaild_page = Grabber.pageUrl(query.page, previous, Number.MAX_VALUE, "{page}", "{max}", "{min}") - 1; // start at 0
+                        const parsed_keyword = search_keyword_parser(query);
+                        return "/api/v1/posts?" + "offset=" + vaild_page + "&tags=" + encodeURIComponent(parsed_keyword["and"]) + "&filter=" + encodeURIComponent(parsed_keyword["filter"]) + "&or=" + encodeURIComponent(parsed_keyword["or"]) + "&unless=" + encodeURIComponent(parsed_keyword["unless"]) + "&mime-type=" + "" + "&order=" + "desc"; // query.page is deprecated and should be replaced with opts.page in the future.
+                    } catch (e: any) {
+                        return { error: e.message };
+                    }
+                },
+                parse: (src: string): IParsedSearch | IError => {
+                    const data = JSON.parse(src);
+                    if (data["Error"]) {
+                        return { error: data["Error"] + '(' + data["Code"] + ')'};
+                    }
+                    const image_count = parseInt(data["TotalPosts"], 10);
+                    const images: IImage[] = [];
+                    for (let i = 0; i < data["Posts"].length; i++) {
+                        images.push(jason_to_iimage(data["Posts"][i]));
+                    }
+                    images.forEach(fill_rating_from_string_tag);
+                    images.forEach(fill_placeholder_preview);
+                    return {
+                        images: images,
+                        imageCount: image_count,
+                        pageCount: Math.ceil(image_count / 50),
+                    };
                 },
             },
         },
